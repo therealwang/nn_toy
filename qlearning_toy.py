56cd24dc226595fa4ed92e918bb5adb7bd872490
Yu Wang <yuwang1@wharton.upenn.edu>
2017-12-28 09:14:25 -0500

    added game/interface to train on

--- qlearning_toy.py
+++ qlearning_toy.py
@@ -6,27 +6,68 @@ Created on Wed Dec 27 21:49:53 2017
 """
 
 import numpy as np
+from msvcrt import getch
+import sys
 
 class Game:
-    def __init__(self, size, init = -1, goal = -1):
-        self.iter = 0
+    def __init__(self, player, size, init = -1, goal = -1):
+        self.player = player
+        self.score = 0
         self.size = size
-        if init == -1:
-            self.init = np.randint(1, self.size)
         if goal == -1:
             self.goal = self.size/2
+        if init == -1:
+            self.init = np.random.randint(1, self.size)
+        self.player.position = self.init
+        self.moves = 0
+        
+    def initboard(self):
+        board = ['.'] * self.size
+        board[0] = 'O'
+        board[self.player.position] = '+'
+        board[self.goal] = '#'
+        return board
+        
+        
+    def startgame(self):
+        self.score = 0
+        self.moves = 0
+        self.displaygrid()
+        while np.abs(self.score) < 3:
+            move = self.player.move()
+            if move == 'a':
+                self.player.position -= 1
+                self.moves += 1
+            elif move == 'd' and self.player.position != self.size - 1:
+                self.player.position += 1
+                self.moves += 1
+            if self.player.position == self.goal:
+                self.score += 1
+                self.player.position = self.init
+            elif self.player.position == 0:
+                self.score -= 1
+                self.player.position = self.init
+            self.displaygrid()
+        print "\nScore: {0}, Moves: {1}".format(self.score, self.moves)
+        
+    def displaygrid(self):
+        sys.stdout.flush()
+        sys.stdout.write('\r' +  ''.join(self.initboard()) + '    Score: {0}'.format(self.score))
 
 class Player:
     def __init__(self):
-        self.score = 0
+        self.position = 0
         
-    def move(self, inp):
-        if inp == 'a':
+    def move(self):
+        inp = ord(getch())
+        if inp == 97:
             return 'a'
-        elif inp == 'd':
+        elif inp == 100:
             return 'd'
         else:
             return
             
 class qPlayer:
     #comment
+    def __init__(self):
+        self.position = 0
